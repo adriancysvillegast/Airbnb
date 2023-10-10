d1a374c11bba8bb61f12028bb7d9c4fe97ff0f07
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-10-10 16:35:51 -0400

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Adriancys Jesus Villegas Toro on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
